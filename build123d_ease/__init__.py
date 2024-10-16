11a83b663dfa0f8b7f12cb0ef14b2375ec5036bc
KilowattSynthesis <180148956+KilowattSynthesis@users.noreply.github.com>
2024-10-16 14:22:33 -0600

    Add rotations and tests

--- build123d_ease/__init__.py
+++ build123d_ease/__init__.py
@@ -1,9 +1,10 @@
 """Extensions, tools, and shortcuts to make modelling with Build123d easier."""
 
-from . import align
+from . import align, rotation
 from .ci_show import show
 
 __all__ = [
     "align",
+    "rotation",
     "show",
 ]
--- tests/test_demo.py
+++
@@ -1,9 +0,0 @@
-"""General placeholder test file."""
-
-
-def test_always_passes() -> None:
-    """Will always pass.
-
-    Used to check that the test suite is working.
-    """
-    assert True
